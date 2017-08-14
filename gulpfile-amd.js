fbb1acde9082bc88b35b2e814b2fa349cf617e50
hell007 <1048523306@qq.com>
2017-08-14 11:05:09 +0800

    Update gulpfile-amd.js

--- gulpfile-amd.js
+++ gulpfile-amd.js
@@ -15,9 +15,12 @@ var concat = require('gulp-concat'),           //合并
 	htmlmin = require('gulp-htmlmin'),
 	clean = require('gulp-clean'),
 	md5 = require('gulp-md5-plus'),
+	rev = require('gulp-rev'),
+	revCollector = require('gulp-rev-collector'),
+	runSequence = require('run-sequence'),
+	useref = require('gulp-useref'),
+	revReplace = require('gulp-rev-replace'),
 	requirejsOptimize = require('gulp-requirejs-optimize');   //require优化
-
-
 
 
 //脚本检查
@@ -26,6 +29,13 @@ gulp.task('lint', function () {
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+
+//清空文件夹，避免资源冗余
+gulp.task('clean',function(){
+    return gulp.src('dist/',{read:false}).pipe(clean());
+});
+
 
 //资源拷贝
 gulp.task('copy:html', function (done) {
@@ -41,11 +51,13 @@ gulp.task('copy:config', function (done) {
 });
 
 
-//压缩html
-gulp.task('minifyhtml', function() {
-  return gulp.src('src/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+//css文件压缩，更改版本号，并通过rev.manifest将对应的版本号用json表示出来
+gulp.task('minifycss',function(){
+    return gulp.src('./src/css/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('dist/css'))
 });
 
 
@@ -68,7 +80,9 @@ gulp.task('index', function () {
 });
 
 
+
 //多页面合并
+//js文件压缩，更改版本号，并通过rev.manifest将对应的版本号用json表示出
 gulp.task('minifyjs', function () {
     gulp.src('./src/js/page/*.js')
         .pipe(requirejsOptimize({ //require config
@@ -78,27 +92,68 @@ gulp.task('minifyjs', function () {
                 'underscore'
             ]
         }))
-        .pipe(uglify())                     //压缩
+        .pipe(uglify()) //压缩
+        .pipe(rev())
+        .pipe(gulp.dest('dist/js/page/'))
+        .pipe(rev.manifest())
         .pipe(gulp.dest("dist/js/page/")); //输出保存
 });
 
-//将js加上10位md5
-gulp.task('md5:js', ['minifyjs'], function (done) {
-    gulp.src('dist/js/page/*.js')
-        .pipe(gulp.dest('dist/js/page/'))
-        .on('end', done);
+
+//将所有的html文件复制到生产环境中去
+gulp.task('minifyhtml', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: false,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: false,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: false,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('dist/**/*.html')
+    	.pipe(htmlmin(options))
+        .pipe(gulp.dest('dist/'));
 });
 
 
-////单页面
-//gulp.task('default', function () {
-//  gulp.run('lint', 'index');
-//});
+//通过hash来精确定位到html模板中需要更改的部分,然后将修改成功的文件生成到指定目录
+gulp.task('rev',function(){
+    return gulp.src(['dist/js/page/*.json','dist/css/*.json','./src/*.html'])
+    .pipe( revCollector({
+        replaceReved: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+//开发下
+gulp.task('devjs', function () {
+    gulp.src('./src/js/page/*.js')
+        .pipe(requirejsOptimize({ //require config
+            mainConfigFile: 'src/js/config.js',
+            exclude: [
+                'jquery',
+                'underscore'
+            ]
+        }))
+        .pipe(uglify()) //压缩
+        .pipe(gulp.dest("dist/js/page/")); //输出保存
+});
+
+gulp.task('devcss',function(){
+    gulp.src('./src/css/**/*.css')
+    	.pipe(gulp.dest('dist/css'))
+});
 
 
 //开发 默认的default任务  gulp
-gulp.task('default',['copy:html','copy:jslib','copy:config','minifyjs']);
+gulp.task('default',['copy:html','copy:jslib','copy:config','devjs','devcss']);
 
 
 //部署
-gulp.task('build',['minifyhtml','copy:jslib','copy:config','minifyjs']);
+gulp.task('build',function(){
+	// 设置队列
+	runSequence('clean',['copy:jslib','copy:config','minifyjs','minifycss'],'rev','minifyhtml');
+});
